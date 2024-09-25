7a3870b7d9059730122a8895bc704547a84e9644
Victor <mwendwav1789@gmail.com>
2024-09-25 12:37:31 +0300

    update index.html

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; // Use backticks for interpolation
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`; // Uppercase the user name
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; // Get the first initial
